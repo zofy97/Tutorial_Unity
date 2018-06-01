8ebac8c2f62478827dfa85c16baaf72455624152
Zofy <32739550+zofy97@users.noreply.github.com>
2018-06-01 16:37:00 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float speed = 0.05f;
+    private int runden = 0;
+    public Text rundenDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position += transform.forward * vertical * speed;
+        transform.Rotate(transform.up * horizontal);
+        //Debug.Log(vertical);
+	}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        runden++;
+        rundenDisplay.text = "Runden: " + runden;
+        //Debug.Log("Collision detected");
+    }
+}
